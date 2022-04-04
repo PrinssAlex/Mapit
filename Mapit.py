b0e5da39857992c896648780aea4e8b9acd6be75
PrinssAlex <prialex477@gmail.com>
2022-04-04 00:44:29 -1100

    Added layer control to map

--- Mapit.py
+++ Mapit.py
@@ -1,6 +1,7 @@
 #import needed libraries
 import pandas as pd
 import folium as fl
+from folium.plugins import BeautifyIcon
 df = pd.read_csv('volcano.txt')  # path to csv file 
 # df should have columns corresponding to the fllg columns or the codes below should be edited to include the respective columns
 lat = list(df['LAT'])
@@ -23,7 +24,9 @@ def el_Icon(elevation):
     else:
         return 'lightgray'
 # map object. Must be created at the start of any map application development
-mappit = fl.Map(location=[48.77, -121.81], zoom_start = 7, tiles='Stamen Terrain')
+mapit = fl.Map(location=[48.77, -121.81], zoom_start = 7, tiles='Stamen Terrain')
+#FeatureGroup method added to group marker layers together
+fg_mkr = fl.FeatureGroup(name='markers')
 # a For-loop to iterate through the df and fill the map object to produce the needed markers on the map
 for lt,ln,el,nm in zip(lat,lon,elv,name):
     html_data = f''' Volcano Name: <a onMouseOver = "this.style.color = 'Tomato'", onMouseOut = "This.style.color = 'Red'", \
@@ -32,7 +35,18 @@ for lt,ln,el,nm in zip(lat,lon,elv,name):
                  <i style = 'display: inline-block; font-size: 12px; color: Gray;'> (click name for more info)</i> <br>
                  Height: {el} m '''
     iframe_obj = fl.IFrame(html=html_data, width=220, height=80) # create's an interactive frame that pop's up on clicking the marker
-    mappit.add_child(fl.Marker(location=[lt, ln], popup=fl.Popup(iframe_obj), icon=fl.Icon(color=el_color(el), icon_color=el_Icon(el), icon='info-sign', prefix='fa'), tooltip=f'mount {nm}')) #create's markers on the map
-
-mappit.save('mapit.html')
+    fg_mkr.add_child(fl.CircleMarker(location=[lt,ln], radius=6, popup=fl.Popup(iframe_obj), fill_color=el_Icon(el), \
+        color=el_color(el), fill_opacity=0.7, tooltip=f'mount {nm}', parse_html=True ) ) 
+#FeatureGroup added to group polygon (mark out territories on the map) layer together - adds a chloropleth feature
+fg_pol = fl.FeatureGroup(name='polygons')
+fg_pol.add_child( fl.GeoJson( data= open('Geo_JSON.json', 'r', encoding='utf-8-sig').read() ,
+style_function=lambda x: {'fillColor':'yellow' if x['properties']['POP2005'] < 1000000 else 'orange' if 
+10000000 <= x['properties']['POP2005'] < 20000000 else 'red' } ) ) #adding a color overlay wrt country populationn using a lambda function
+# Add feature group to map
+mapit.add_child(fg_mkr)
+mapit.add_child(fg_pol)
+# Add layers to map layer control
+mapit.add_child(fl.LayerControl())
+# Save map to local machine
+mapit.save('mapit.html')
 
